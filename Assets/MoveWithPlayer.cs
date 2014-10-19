12058fc494c1b60700db46916bf4b494d5dc4eab
o.glushko <o.glushko@globalplaygames.com>
2014-10-19 19:06:07 +0300

    init

---
+++ Assets/MoveWithPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithPlayer : MonoBehaviour
+{
+    public GameObject target;
+	
+    // Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    this.transform.position = target.transform.position + new Vector3(0, 0, -10);
+	}
+}
