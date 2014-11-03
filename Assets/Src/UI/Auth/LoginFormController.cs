cfa76985da5761f52f57d02f0c36d1692565b63c
o.glushko <o.glushko@globalplaygames.com>
2014-11-03 14:47:06 +0200

    disable login window

---
+++ Assets/Src/UI/Auth/LoginFormController.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Src.Net.Envelopes.Server;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginFormController : MonoBehaviour
+{
+    public Canvas LoginCanvas;
+    public InputField LoginInput;
+    public Button LoginButton;
+    public Animator LoginWindowAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LoginButton.interactable = LoginInput.text != null && LoginInput.text.text.Length > 0;
+	}
+
+    public void sendLoginToServer()
+    {
+        string login = LoginInput.text.text;
+        if (login.Length == 0)
+            return;
+        Debugger.Log("LoginButton.sendLoginToServer " + login);
+        new SNewPlayerEnvelope(login).Send();
+        LoginWindowAnimator.SetBool("LoggedIn", true);
+    }
+
+    public void hideLoginForm()
+    {
+        Debugger.Log("hiding login form...");
+        LoginCanvas.enabled = false;
+
+    }
+
+}
