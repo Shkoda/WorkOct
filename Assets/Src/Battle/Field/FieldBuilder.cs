56be865ca5929f61aabb45053da833efbde3a6a5
o.glushko <o.glushko@globalplaygames.com>
2014-11-11 12:59:53 +0200

    unbreakable block prototype

---
+++ Assets/Src/Battle/Field/FieldBuilder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldBuilder : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+//        this.transform.localScale *= new Vector3(10, 0);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
