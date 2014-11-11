642226ed243b63c66b4f7bcde47784ee7febf125
o.glushko <o.glushko@globalplaygames.com>
2014-11-11 18:18:26 +0200

    destroyable block. destroy bullet.

--- Assets/Src/Battle/Game/Weapon/BulletColliding.cs
+++ Assets/Src/Battle/Game/Weapon/BulletColliding.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class BulletColliding : MonoBehaviour {
+    public int damage;
 
 	// Use this for initialization
 	void Start () {
-	
+        Destroy(gameObject, 20); // 20sec
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+     
+//            Destroy(gameObject); // Remember to always target the game object, otherwise you will just remove the script
+     
+
+
+    }
 }
---
+++ Assets/Src/Battle/HealthScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour
+{
+    public int HP;
+    public bool IsDestroyable;
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void Damage(int damage)
+    {
+        if (IsDestroyable)
+        {
+            HP -= damage;
+            if (HP <= 0)
+                Destroy(gameObject);
+        }    
+    }
+   
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        BulletColliding shot = otherCollider.gameObject.GetComponent<BulletColliding>();
+        if (shot != null)
+        {
+            // Avoid friendly fire
+//            if (shot.isEnemyShot != isEnemy)
+//            {
+                Damage(shot.damage);
+
+                // Destroy the shot
+                Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+//            }
+        }
+
+
+    }
+}
